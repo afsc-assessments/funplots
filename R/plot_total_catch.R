4a2a6e28978928c4dea3c4a4f2cfe6bb72ab8283
careymcg <carey.mcgilliard@gmail.com>
2022-06-29 12:17:10 -0700

    Added .Rmd to make figs and reorganized use of dplyr and ggplot2
    Added Make_Catch_Figures.Rmd to make figures of catches and output figures and captions to word. Added output_template.docx to format .Rmd word output. Changed to ImportFrom dplyr <function> and same for ggplot2. Changed plot_catches.R to plot_total_catch.R to be more specific.

---
+++ R/plot_total_catch.R
@@ -0,0 +1,40 @@
+
+#' Plot catches for each species by gear
+#'
+#' @param year latest year to include
+#' @param species species group code, e.g. "DUSK" found where
+#' @param area sample area "GOA", "AI" or "BS" (or combos)
+#'
+#' @return
+#' @export
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate
+#' @importFrom dplyr filter
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @importFrom dplyr left_join
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_bar
+#' @importFrom ggplot2 geom_col
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 facet_grid
+#' @importFrom ggplot2 labeller
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 scale_color_manual
+#' @importFrom viridis scale_color_viridis
+#' @importFrom ggplot2 scale_fill_discrete
+#' @importFrom ggplot2 labs
+
+#'
+#' @examples
+#' plot_catches(year = 2022,species = "DUSK",area = "GOA")
+
+plot_total_catch<-function(year,species,area,db = akfin) {
+    #pull catch data
+    c.df<-afscdata::q_catch(year=year, species=species, area=area, db = db,save = TRUE)
+    return(c.df)
+
+
+
+}
