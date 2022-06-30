48a684f076cc556973bbb7a553ef98a510a30382
careymcg <carey.mcgilliard@gmail.com>
2022-06-30 12:16:58 -0700

    Completed a plot in plot_total_catch.R
    A bar plot of total catches by gear was added in plot_total_catch.R. Make_Catch_Figures.Rmd works and creates a word doc with this figure.

--- R/plot_total_catch.R
+++ R/plot_total_catch.R
@@ -5,38 +5,35 @@
 #' @param year latest year to include
 #' @param species species group code, e.g. "DUSK" found where
 #' @param area sample area "GOA", "AI" or "BS" (or combos)
-#' @param db database to use
 #'
 #' @return
 #' @export
-#' @importFrom dplyr select
-#' @importFrom dplyr mutate
-#' @importFrom dplyr filter
 #' @importFrom dplyr summarise
 #' @importFrom dplyr group_by
-#' @importFrom dplyr left_join
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 geom_bar
-#' @importFrom ggplot2 geom_col
 #' @importFrom ggplot2 geom_line
-#' @importFrom ggplot2 facet_grid
-#' @importFrom ggplot2 labeller
 #' @importFrom ggplot2 theme
-#' @importFrom ggplot2 scale_color_manual
-#' @importFrom ggplot2 scale_fill_discrete
+#' @importFrom ggplot2 element_text
+#' @importFrom ggplot2 scale_fill_viridis_d
 #' @importFrom ggplot2 labs
 
 #'
 #' @examples
 #' plot_total_catch(year = 2022,species = "DUSK",area = "GOA",db = akfin)
 
-plot_total_catch<-function(thedata,year,species,area,db = akfin) {
+plot_total_catch<-function(thedata,year,species,area) {
     #pull catch data
 
-    csum.df<-thedata %>% group_by(YEAR) %>%
-      summarise(Total = sum(TONS))
-      g<-ggplot(data = csum.df,aes(x = YEAR,y = Total))
+    csum.df<-thedata %>% group_by(Year,Gear) %>%
+      summarise(Total = sum(Tons))
+      g<-ggplot(data = csum.df,aes(x = Year,y = Total,fill = Gear)) +
+        geom_bar(stat = "identity") +
+        theme(axis.text.x = element_text(angle = 55,vjust = 0.75)) +
+        scale_fill_viridis_d(direction = -1) +
+        labs(x="Year",y="Total Catch (mt)")
+       # + theme(axis.title.x = element_text(vjust=-0.5))
 
 return(g)
 }
